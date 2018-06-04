c653301da409769a37f368d8cd583e574801591b
takenoko-str <taku.hyogo.kei@gmail.com>
2018-06-04 10:33:19 +0900

    マジカルアンバー追加

--- main.py
+++ main.py
@@ -2,4 +2,5 @@ from package.master import order
 
 if __name__ == '__main__':
     order.destroy_grail()
+    order.colaboration()
 
--- package/master/order.py
+++ package/master/order.py
@@ -1,8 +1,12 @@
 from ..servant.origin import EmiyaSan
 from ..servant.origin import SaberSan
+from ..outsider import MagicalAmber
 
 EmiyaSan.awake()
 
 def destroy_grail():
     SaberSan.run()
+
+def colaboration():
+    MagicalAmber.hello()
 
